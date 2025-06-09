ec433c2ab39b8b83f3f8b2e69b8604ddcb98e565
Prakash kumar <kumarprakash0196@gmail.com>
2025-06-09 15:54:10 +0530

    Update Register.jsx

--- Client/src/Pages/Register.jsx
+++ Client/src/Pages/Register.jsx
@@ -25,7 +25,7 @@ export const Register = () => {
     console.log(user);
 
     try {
-      const response = await fetch("http://localhost:3000/api/auth/register", {
+      const response = await fetch("https://server-08sc.onrender.com/api/auth/register", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
