2ba623c69bc0b951ab9802763db8064a844086c7
Prakash kumar <kumarprakash0196@gmail.com>
2025-06-25 14:32:53 +0530

    Update Contact.jsx

--- Client/src/Pages/Contact.jsx
+++ Client/src/Pages/Contact.jsx
@@ -93,7 +93,7 @@ export const Contact = ()=>{
             </form>
           </div>
           <div className="contact-image" style={{margin:"20%"}}>
-            <iframe
+{/*             <iframe
   src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3500.86077908089!2d77.0535110760919!3d28.663886975647685!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjjCsDM5JzUwLjAiTiA3N8KwMDMnMjEuOSJF!5e0!3m2!1sen!2sin!4v1748782023806!5m2!1sen!2sin"
   width="600"
   height="450"
@@ -102,7 +102,7 @@ export const Contact = ()=>{
   loading="lazy"
   referrerPolicy="no-referrer-when-downgrade"
 />
-
+ */}
           </div>
         </div>
       </section>
